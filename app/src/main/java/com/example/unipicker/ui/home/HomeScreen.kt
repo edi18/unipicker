f3376ab7c22806dda68ef9d40a5f764f7e80875c
Edim Omerhodzic <omerhodzic.edim@outlook.com>
2023-06-09 13:07:31 +0200

    fix gradle

--- app/src/main/java/com/example/unipicker/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/unipicker/ui/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.unipicker.ui.home
 
+import androidx.compose.runtime.Composable
+
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
@@ -21,10 +23,9 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
