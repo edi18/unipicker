0ba2fd958e53fab3877ba88d5ae05317af2ed6fe
josip <josiprados.biz@gmail.com>
2023-06-09 12:04:06 +0200

    Home screen and gradle

--- app/src/main/java/com/example/unipicker/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/unipicker/ui/home/HomeScreen.kt
@@ -1,7 +1,110 @@
 package com.example.unipicker.ui.home
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.unipicker.R
+
 
 @Composable
 fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 20.dp, bottom = 200.dp, start = 50.dp, end = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Koji fakultet je ZA TEBE??",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        val imageModifier = Modifier
+            .size(300.dp)
+            .clip(shape = CircleShape)
+        val image: Painter = painterResource(id = R.drawable.naslovna) // Replace with your image resource
+
+        Image(
+            painter = image,
+            contentDescription = "Image",
+            modifier = imageModifier,
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        AnimatedButton()
+    }
+}
+
+@Composable
+fun AnimatedButton() {
+    var isButtonUp by remember { mutableStateOf(false) }
+
+    val animatedValue by animateDpAsState(
+        targetValue = if (isButtonUp) 0.dp else 16.dp,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    LaunchedEffect(Unit) {
+        isButtonUp = true
+    }
+
+    Button(
+        onClick = {
+            isButtonUp = !isButtonUp
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+            .offset(y = animatedValue)
+    ) {
+        Text(
+            text = "Započni kviz",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
 }
